1430555a77b81df5cb2b6ab8f6a6fc08f331297a
myubunto <saeedsamadyar@yahoo.com>
2022-08-16 01:20:07 -0700

    8/15/2022

--- Session17_Permissions.py
+++ Session17_Permissions.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from time import sleep
-from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 service = Service(executable_path=ChromeDriverManager().install())
