eba40e5948e53463dbbfe8877573913d5669a1ae
saeed s <saeedsamadyar@yahoo.com>
2022-08-19 11:34:15 +0430

    11:34 PM Aug 8/19/2022

--- SessionOne-Three/06_WorkingWithElements.py
+++ SessionOne-Three/06_WorkingWithElements.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
+
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 driver.get("http://wikipedia.com")
 
@@ -9,30 +10,30 @@ driver.get("http://wikipedia.com")
 el1 = driver.find_element('id', 'searchInput')
 el1.send_keys("Hello world!")
 sleep(1)
-##
+#
 # 2 > Xpath
-# Absolute
+#٫ Absolute
 el2 = driver.find_element('xpath', '/html/body/div[3]/form/fieldset/div/input')
 print(el1)
 print(el2)
 assert el1 == el2
 # Relative
-driver.find_element('xpath', "//input[@type='search']")
+#driver.find_element('xpath', "//input[@type='search']")
 driver.find_element('xpath', "//*[text()='Italiano']")
 
 
 # 3 > Tag
-driver.find_element('tag name', 'select')
+#driver.find_element('tag name', 'select')
 
 # 4 > Link Text
-driver.find_element('link text', 'Download Wikipedia for Android or iOS')
+#driver.find_element('link text', 'Download Wikipedia for Android or iOS')
 
 # 5 > Partial Link Text
-driver.find_element('partial link text', 'Download')
+#driver.find_element('partial link text', 'Download')
 
 # 6 > Class
-driver.find_element('class name', 'svg-search-icon')
+#driver.find_element('class name', 'svg-search-icon')
 
 # 7 > Css Selector
-driver.find_element('css selector', '#searchInput')
-driver.find_element('css selector', '.svg-search-icon')
+#driver.find_element('css selector', '#searchInput')
+#driver.find_element('css selector', '.svg-search-icon')
